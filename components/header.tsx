1f3585de81287e4e5a9966c2209b227dbefd0692
LRasile <rasile.leonardo@gmail.com>
2020-11-22 20:41:53 +0000

    navbar updates

--- components/header.tsx
+++ components/header.tsx
@@ -1,68 +1,43 @@
 
 import Link from 'next/link';
 import { signin, signout, useSession } from 'next-auth/client';
-
-const name = 'Leonardo Rasile'
+import { Nav, Navbar, NavDropdown, Container, Button, Image } from 'react-bootstrap';
 
 export default function Header({ home }: { home?: boolean }) {
 
   const [session, loading] = useSession();
 
+  const avatar = (
+    <Image src={session?.user?.image}
+      style={{ height: '2rem', width: '2rem', marginRight: '0.5rem' }}
+      roundedCircle />
+  )
+
+  const signInOutButton = session ?
+    (
+      <NavDropdown title={avatar} id="collasible-nav-dropdown">
+        <NavDropdown.Item href="/profile">
+          Profile
+        </NavDropdown.Item>
+        <NavDropdown.Item href="/api/auth/signout" onClick={(e) => { e.preventDefault(); signout(); }}>
+          Sign Out
+        </NavDropdown.Item>
+      </NavDropdown>
+    ) :
+    (
+      <Nav.Link href="/api/auth/signin" onClick={(e) => { e.preventDefault(); signin(); }}>
+        <Button variant="outline-primary">Sign in</Button>
+      </Nav.Link>
+    );
+
   return (
     <header>
-      <nav className='navbar fixed-top navbar-expand-lg navbar-dark bg-dark'>
-        <div className='container'>
-          <a className="navbar-brand" href="/">Navbar</a>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav ml-auto mr-1">
-              {!session && (
-                <li className="nav-item">
-                  <a className="nav-link"
-                    href="/api/auth/signin"
-                    onClick={(e) => {
-                      e.preventDefault();
-                      signin();
-                    }}
-                  >
-                    <button className="btn btn-primary">Sign in</button>
-                  </a>
-                </li>
-              )}
-              {session && (
-                <>
-                  <li className="nav-item">
-                    <Link href="/profile">
-                      <a className="nav-item">
-                        <span
-                          style={{ backgroundImage: `url(${session.user.image})` }}
-                          className="avatar"
-                        />
-                        <span className="nav-link" >{session.user.email} </span>
-                      </a>
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <a className="nav-item"
-                      href="/api/auth/signout"
-                      onClick={(e) => {
-                        e.preventDefault();
-                        signout();
-                      }}
-                    >
-                      <button className="btn btn-outline-primary">Sign out</button>
-                    </a>
-                  </li>
-                </>
-              )}
-            </ul>
-          </div>
-        </div>
-      </nav>
-
+      <Navbar fixed="top" collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="/">Navbar</Navbar.Brand>
+          <Nav className="navbar-nav ml-auto mr-1">{signInOutButton}</Nav>
+        </Container>
+      </Navbar>
     </header >
   );
 };
